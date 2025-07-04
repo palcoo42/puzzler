af32a66abd1126602340b9d97634b337b0f204a0
palcoo42 <122668559+palcoo42@users.noreply.github.com>
2025-07-04 22:00:18 +0000

    Fix template to use new() method to instantiate solution

--- template/src/main.rs
+++ template/src/main.rs
@@ -7,7 +7,7 @@ use crate::puzzle::solution::Solution;
 mod puzzle;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let puzzle = Box::new(Solution {});
+    let puzzle = Box::new(Solution::new());
     let mut solver = Solver::new(puzzle, 2);
     solver.run()
 }
