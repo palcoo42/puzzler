f1a9fefa308db94f5f4ed85fb8db5b809479af21
palcoo42 <pavol.ondrusek@gmail.com>
2025-07-26 12:49:14 +0200

    Add method Point::neighbor_at()

--- src/grids/point.rs
+++ src/grids/point.rs
@@ -23,6 +23,19 @@ impl Point {
             Direction::NorthWest => Self::new(self.x - 1, self.y - 1),
         }
     }
+
+    pub fn neighbor_at(&self, direction: &Direction, distance: isize) -> Self {
+        match direction {
+            Direction::North => Self::new(self.x, self.y - distance),
+            Direction::NorthEast => Self::new(self.x + distance, self.y - distance),
+            Direction::East => Self::new(self.x + distance, self.y),
+            Direction::SouthEast => Self::new(self.x + distance, self.y + distance),
+            Direction::South => Self::new(self.x, self.y + distance),
+            Direction::SouthWest => Self::new(self.x - distance, self.y + distance),
+            Direction::West => Self::new(self.x - distance, self.y),
+            Direction::NorthWest => Self::new(self.x - distance, self.y - distance),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -41,5 +54,43 @@ mod tests {
         assert_eq!(point.neighbor(&Direction::SouthWest), Point { x: 4, y: 4 });
         assert_eq!(point.neighbor(&Direction::West), Point { x: 4, y: 3 });
         assert_eq!(point.neighbor(&Direction::NorthWest), Point { x: 4, y: 2 });
+    }
+
+    #[test]
+    fn test_neighbor_at() {
+        let point = Point { x: 5, y: 3 };
+
+        assert_eq!(
+            point.neighbor_at(&Direction::North, 7),
+            Point { x: 5, y: -4 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::NorthEast, 7),
+            Point { x: 12, y: -4 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::East, 7),
+            Point { x: 12, y: 3 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::SouthEast, 7),
+            Point { x: 12, y: 10 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::South, 7),
+            Point { x: 5, y: 10 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::SouthWest, 7),
+            Point { x: -2, y: 10 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::West, 7),
+            Point { x: -2, y: 3 }
+        );
+        assert_eq!(
+            point.neighbor_at(&Direction::NorthWest, 7),
+            Point { x: -2, y: -4 }
+        );
     }
 }
